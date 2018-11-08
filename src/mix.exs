f00ebfcf11e2a6414fd54c10676d44a6fd615fe0
hykw <hykw1234@gmail.com>
2018-11-08 13:23:43 +0900

    update phoenix: 1.4.0-rc.3 -> 1.4.0

--- src/mix.exs
+++ src/mix.exs
@@ -33,7 +33,7 @@ defmodule Showcase.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.0-rc", override: true},
+      {:phoenix, "~> 1.4"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 4.0"},
       {:mariaex, ">= 0.0.0"},
